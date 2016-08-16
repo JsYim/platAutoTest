f5f990a7eda1c0a65c6c5370180503e84ba45bb0
JsYim <412463539@qq.com>
2016-08-16 23:53:15 +0800

    Update area_sort.rb

--- area_sort.rb
+++ area_sort.rb
@@ -1,26 +1,43 @@
-
-def graphic_sort(sort, parameter)
+def graphic_dimension (sort, *var)
   if sort == "circle"
-    dimension = cirlce_dimension(parameter)
-    elsif sort == "triangle"
-      dimension = trianlge_dimension(parameter)
-      elsif sort
-end
-
+    dimension = cirlce_dimension(*var)
+  elsif sort == "triangle"
+    dimension = triangle_dimension(*var)
+  elsif sort == "rectangle"
+    dimension = rectangle_dimension(*var)
   else
-    dimension =
+    dimension = 0
   return dimension
+  end
 end
-
 
 def cirlce_dimension radius
-  return  3 *radius * radius
+  return  3 *radius.to_i * radius.to_i
 end
 
-def triangle_dimension radius
-  return  3 *radius * radius
+def triangle_dimension width, height
+  return  width.to_i * height.to_i / 2
 end
 
+def rectangle_dimension width, height
+  return  width.to_i* height.to_i
+end
 
-result = graphic_sort("circle",5)
-puts ("Dimension: #{result}")
+def graphic_dimension_sort dimension
+  return dimension.sort
+end
+result = Array.new
+
+result[0] = graphic_dimension("circle",5)
+result[1]  = graphic_dimension("triangle", 5, 10)
+result[2]  = graphic_dimension("rectangle", 8, 9)
+
+final = graphic_dimension_sort(result)
+
+puts ("Dimension of circle: #{result[0]}")
+puts ("Dimension of triangle: #{result[1]}")
+puts ("Dimension of rectangle: #{result[2]}")
+puts ("Sort of graphic dimensions: #{final}")
+
+arr = IO.readlines("testfile")
+puts arr[0]
